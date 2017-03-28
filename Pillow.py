a8c73133560c117037572ce9f115b9127a048973
宗吾先生 <zyd13750026208@outlook.com>
2017-03-28 11:11:33 +0800

    Update Pillow.py

--- Pillow.py
+++ Pillow.py
@@ -1,6 +1,8 @@
 #图片处理pillow模块的应用，
-#参考两个网址：http://www.w2bc.com/article/122079
-#http://python.jobbole.com/84956/
+#参考3个网址：
+http://www.w2bc.com/article/122079
+http://python.jobbole.com/84956/
+http://www.cnblogs.com/way_testlife/archive/2011/04/17/2019013.html
 
 from PIL import Image
 #打开图片
