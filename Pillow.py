7e9867546880c1a0462e4fd65cb5f0fc4905cbcb
宗吾先生 <zyd13750026208@outlook.com>
2017-03-28 13:42:11 +0800

    Update Pillow.py

--- Pillow.py
+++ Pillow.py
@@ -7,3 +7,7 @@ http://www.cnblogs.com/way_testlife/archive/2011/04/17/2019013.html
 from PIL import Image
 #打开图片
 im = Image.open(path)
+
+#图片截取imf
+img.crop((x1, x2, x3, x4))
+crop函数带的参数为(起始点的横坐标，起始点的纵坐标，宽度，高度）
